58889c52d1dd7006a0b9cf51c1e0b247e2a81dad
AkkafaOguz <88987698+AkkafaOguz@users.noreply.github.com>
2023-12-24 11:10:40 +0300

    Day#7 and Day#8

---
+++ C#/C#/Intermediate/5-Interface/Interface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Intermediate._5_Interface
+{
+    internal class Interface
+    {
+        //Open-Close Principle or OCP -> Open for extension but closed for modification
+
+        //Interfaces are used for building loosely coupled extensible and testable applications not for code reusability
+
+        //What interfaces do provide polymorphic behavior
+    }
+}
