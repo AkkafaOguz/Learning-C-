07b2be5267909a0885ef30d63d893b35d459fe0c
AkkafaOguz <88987698+AkkafaOguz@users.noreply.github.com>
2023-12-26 19:32:18 +0300

    Day#8 and Day#9

---
+++ C#/C#/3-C# Fundamentals/Class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_._3_C__Fundamentals
+{
+    internal class Class
+    {
+
+        // Switch case are not working with double and float value types
+        
+        // Default parameters can be used for methods
+
+        public static void DefaultParameter(int number, int number2 = 1)
+        {
+            Console.WriteLine("Normal parameter: {0} and Default parameter: {1}",number,number2);
+        }
+
+        // By using 'Named Arguments' order of arguments that send to the method are not important
+
+        public static void NamedArguments(int number, int number2, string str)
+        {
+            Console.WriteLine("Order of parameters: {0},{1},{2}",number,number2,str);
+        }
+
+        //Expression Body syntax
+
+        public static void ExpressionBodySyntax(int number) => 
+            Console.WriteLine("Normal parameter: {0}", number);
+
+        //static void Main(string[] args)
+        //{
+        //    DefaultParameter(0);
+        //    NamedArguments(str: "Named Arguments", number2: 20, number:1);
+        //}
+    }
+}
---
+++ C#/C#/3-C# Fundamentals/String.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_._3_C__Fundamentals
+{
+    internal class String
+    {
+        static void Main(string[] args)
+        {
+            var name = "Oguz";
+            var age = 28;
+
+            // String Format
+
+            var sentence = string.Format("Hi {0}, you're {1} years old.", name, age);
+            Console.WriteLine(sentence);
+
+            // String Interpolation
+
+            var sentence2 = $"Hi {name}, you're {age} years old.";
+            Console.WriteLine(sentence2);
+
+            // Verbatim Strings
+
+            var path = "C:\\Documents\\readme.txt";
+            var path2 = @"C\Documents\readme.txt";  // <- Verbatim strings
+
+            // -> \" for double quote
+
+            var example = "This is \"the best\" part";
+            Console.WriteLine(example);
+
+            // Try Parse
+
+            var tryPar = "x";
+            int tryParNumber;
+            Console.WriteLine(int.TryParse(tryPar, out tryParNumber));
+
+            // Method parameters (ref)
+
+            var first = 2;
+            var second = 3;
+            var third = 4; // <- must be initialized 
+            checkParametersRef(first,second,ref third);
+            Console.WriteLine("first => " + first);
+            Console.WriteLine("second => " + second);
+            Console.WriteLine("third => " + third);
+
+            // Method parameters (out)
+            int outKey;
+            checkParametersOut(first,out outKey);
+            Console.WriteLine("outKey =>" + outKey);
+
+        }
+
+        // Method parameters (ref)
+        private static void checkParametersRef(int a, int b)
+        {
+            a++;
+            b++;
+        }
+
+        private static void checkParametersRef(int a, int b, ref int c)
+        {
+            a++;
+            b++;
+            c++;
+        }
+
+        // Method parameters (out)
+        private static void checkParametersOut(int a, out int b)
+        {
+            b = 0;
+            a++;
+            b++;
+        }
+    }
+}
